14b7e379ceeb441b8aef3a48eecee8061367c819
ynie88 <ynie.88@gmail.com>
2017-08-30 08:01:24 -0400

    modified tableview

--- SnapKit.playground/Contents.swift
+++ SnapKit.playground/Contents.swift
@@ -4,47 +4,69 @@ import UIKit
 import PlaygroundSupport
 import SnapKit
 
-
-var str = "Hello, playground"
-let view = UIView(frame: CGRect(x: 0, y: 0, width: 400, height: 600))
-view.backgroundColor = .white
-
 PlaygroundPage.current.needsIndefiniteExecution = true
-PlaygroundPage.current.liveView = view
-let label:UILabel = {
-    let label = UILabel()
-    label.text = str
-    label.textAlignment = .center
-    return label
-}()
 
-view.addSubview(label)
-
-label.snp.updateConstraints { (make) in
-    make.top.equalTo(view).offset(10)
-    make.centerX.equalTo(view)
+class CustomCell:UITableViewCell {
+    static let reuseID = "CustomCellREUSEID"
+    private lazy var label:UILabel = {
+        let label = UILabel()
+        self.contentView.addSubview(label)
+        return label
+    }()
+    
+    public func configure(with text:String) {
+        label.text = text
+    
+        setNeedsUpdateConstraints()
+        updateConstraintsIfNeeded()
+    }
+    
+    override func updateConstraints() {
+        //what to do with the constraints?
+        
+        super.updateConstraints()
+    }
 }
 
-let tableview = UITableView(frame: CGRect(x: 0, y: 0, width: 400, height: 700))
-
+class CustomTableView:UITableView {
+    override init(frame: CGRect, style: UITableViewStyle) {
+        super.init(frame: frame, style: style)
+        tableview.register(CustomCell.self, forCellReuseIdentifier: CustomCell.reuseID)
+        tableview.dataSource = TableViewDataSource()
+        
+        //do I need to add anything else here?
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    
+}
 
 class TableViewDataSource:NSObject, UITableViewDataSource {
-    
+    let sampleText:[String] = ["Text test Text test Text test Text test ","Text test "]
     
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 2
+        return sampleText.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        
+        let cell = tableview.dequeueReusableCell(withIdentifier: CustomCell.reuseID, for: indexPath) as! CustomCell
+        
+        cell.configure(with: sampleText[indexPath.row])
+        
+        return cell
     }
 }
-
+let frame = CGRect(x: 0, y: 0, width: 400, height: 700)
+let tableview = CustomTableView(frame: frame , style: .plain)
 tableview.dataSource = TableViewDataSource()
 
 
-//PlaygroundPage.current.liveView = tableview
+PlaygroundPage.current.liveView = tableview
